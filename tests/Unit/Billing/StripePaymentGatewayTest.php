41c476a238164a2b66fb338747f091dfac5e5506
IlijaT <tatalovicilija@gmail.com>
2019-07-29 19:46:17 +0200

    Installed Stripe php library

---
+++ tests/Unit/Billing/StripePaymentGatewayTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\Billing;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class StripePaymentGatewayTest extends TestCase
+{
+
+   /** @test */
+   public function charges_with_a_valid_payment_token_are_successful()
+   {
+    // New PaymentGateway instance
+    $paymentGateway = new StripePaymentGateway;
+    // api_key treba da se dobija iz config('services.stripe.secret')
+
+    $token = \Stripe\Token::create([
+        'card' => [
+          'number' => '4242424242424242',
+          'exp_month' => 1,
+          'exp_year' => date('Y') + 1,
+          'cvc' => '123'
+        ]
+        ], ['api_key' => "sk_test_4eC39HqLyjWDarjtT1zdp7dc"] )->id;
+
+    // Create a new charge with a paymentGateWay valid token
+    $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+
+    // verity that the charge was created successfullly
+    $lastCharge = \Stripe\Charge::all(
+        ["limit" => 1],
+        ['api_key' => "sk_test_4eC39HqLyjWDarjtT1zdp7dc"]
+        )['data'][0];
+
+    $this->assertEquals(2500, $lastCharge->amount);
+   }
+}
