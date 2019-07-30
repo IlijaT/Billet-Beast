2ca4bf02670f011f83466bd6ee1de2da903d81f9
IlijaT <tatalovicilija@gmail.com>
2019-07-30 17:32:20 +0200

    StripePaymentGatewayTest retrives the last charge only if charge is happened

--- tests/Unit/Billing/StripePaymentGatewayTest.php
+++ tests/Unit/Billing/StripePaymentGatewayTest.php
@@ -10,32 +10,59 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class StripePaymentGatewayTest extends TestCase
 {
 
-   /** @test */
-   public function charges_with_a_valid_payment_token_are_successful()
-   {
-    // New PaymentGateway instance
-    $paymentGateway = new StripePaymentGateway('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-    // api_key treba da se dobija iz config('services.stripe.secret')
+    private function lastCharge()
+    {
+        return \Stripe\Charge::all(
+            ["limit" => 1],
+            ['api_key' => config('services.stripe.secret')]
+            )['data'][0];
+    }
 
-    $token = \Stripe\Token::create([
+    private function newCharges()
+    {
+        return \Stripe\Charge::all(
+            [
+                'limit' => 1,
+                'ending_before' => $this->lastCharge->id
+            ],
+            ['api_key' => config('services.stripe.secret')]
+            )['data'];
+        
+    }
+
+    private function validToken()
+    {
+       return \Stripe\Token::create([
         'card' => [
           'number' => '4242424242424242',
           'exp_month' => 1,
           'exp_year' => date('Y') + 1,
           'cvc' => '123'
         ]
-        ], ['api_key' => "sk_test_4eC39HqLyjWDarjtT1zdp7dc"] )->id;
+        ], ['api_key' => config('services.stripe.secret')] )->id; 
+    }
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        $this->lastCharge = $this->lastCharge();
+    }
+
+   /** @test */
+   public function charges_with_a_valid_payment_token_are_successful()
+   {
+
+    // New PaymentGateway instance
+    $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
 
     // Create a new charge with a paymentGateWay valid token
-    $paymentGateway->charge(2500, $token);
+    $paymentGateway->charge(2500, $this->validToken());
 
     // verity that the charge was created successfullly
 
-    $lastCharge = \Stripe\Charge::all(
-        ["limit" => 1],
-        ['api_key' => "sk_test_4eC39HqLyjWDarjtT1zdp7dc"]
-        )['data'][0];
 
-    $this->assertEquals(2500, $lastCharge->amount);
+    $this->assertCount(1, $this->newCharges());
+    $this->assertEquals(2500, $this->lastCharge()->amount);
    }
 }
