00733b46beaafab3d73364360241cc90431163e1
IlijaT <tatalovicilija@gmail.com>
2019-08-02 19:07:44 +0200

    OrderConfirmationNumber Facade binded to OrderConfirmationNumberGenerator interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use App\Billing\PaymentGateway;
 use App\Billing\StripePaymentGateway;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use App\OrderConfirmationNumberGenerator;
+use App\RandomOrderConfirmationNumberGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
         $this->app->bind(PaymentGateway::class, StripePaymentGateway::class);
+        $this->app->bind(OrderConfirmationNumberGenerator::class, RandomOrderConfirmationNumberGenerator::class);
     }
 
     /**
