faf22f911da5ecf77f7ecd098e6da4881538fe1a
IlijaT <tatalovicilija@gmail.com>
2019-08-16 22:04:46 +0200

    Promoters can view only their own concerts

--- tests/Feature/Backstage/ViewConcertListingTest.php
+++ tests/Feature/Backstage/ViewConcertListingTest.php
@@ -23,19 +23,25 @@ class ViewConcertListingTest extends TestCase
     }
 
     /** @test */
-    public function promoters_can_view_a_list_of_their_concerts()
+    public function promoters_can_only_view_a_list_of_their_own_concerts()
     {
         $this->withoutExceptionHandling();
 
         $user = factory(User::class)->create();
+        $otherUser = factory(User::class)->create();
 
+        $concertA = factory(Concert::class)->create(['user_id' => $user->id]);
+        $concertB = factory(Concert::class)->create(['user_id' => $user->id]);
+        $concertC = factory(Concert::class)->create(['user_id' => $otherUser->id]);
+        $concertD = factory(Concert::class)->create(['user_id' => $user->id]);
 
-        $concerts = factory(Concert::class, 3)->create(['user_id' => $user->id]);
 
         $response = $this->actingAs($user)->get('/backstage/concerts');
         $response->assertStatus(200);
-        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[0]));
-        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[1]));
-        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[2]));
+
+        $this->assertTrue($response->original->getData()['concerts']->contains($concertA));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concertB));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concertD));
+        $this->assertFalse($response->original->getData()['concerts']->contains($concertC));
     }
 }
